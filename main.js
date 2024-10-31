e948ccf7c10237a5cf5b5ccf0578914cc3a706f9
alvarodev8 <alvarodev8@gmail.com>
2024-10-31 09:20:07 +0100

    Add correct class to HTML

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ const initialize = () => {
   const divContainer = divs[0]
   const divSearch = divs[1]
   const btnAdd = document.querySelector("button")
-  const divEmpty = divs[2]
-  const divTaskCount = divs[3]
+  const divLiContainer = divs[2]
+  const divEmpty = divs[3]
+  const divTaskCount = divs[4]
   
   divContainer.classList.add("container")
   divSearch.classList.add("search")
   btnAdd.classList.add("btn-add")
+  divLiContainer.classList.add("li-container")
   divEmpty.classList.add("empty")
   divTaskCount.classList.add("task-count")
 
